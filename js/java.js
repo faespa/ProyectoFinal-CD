340c855e1aeab63b3f4c154026fcebf432015895
Faespa <faespa_lia@hotmail.com>
2022-08-09 19:20:02 -0500

    adding BSTP

---
+++ js/java.js
@@ -0,0 +1,46 @@
+
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl, {
+    autohide: false
+  }).show()
+})
+
+
+const myCarousel = document.getElementById("carouselExampleIndicators");
+const carouselIndicators = myCarousel.querySelectorAll(
+  ".carousel-indicators button span"
+);
+let intervalID;
+
+const carousel = new bootstrap.Carousel(myCarousel);
+
+window.addEventListener("load", function () {
+  fillCarouselIndicator(1);
+});
+
+myCarousel.addEventListener("slide.bs.carousel", function (e) {
+  let index = e.to;
+  fillCarouselIndicator(++index);
+});
+
+function fillCarouselIndicator(index) {
+  let i = 0;
+  for (const carouselIndicator of carouselIndicators) {
+    carouselIndicator.style.width = 0;
+  }
+  clearInterval(intervalID);
+  carousel.pause();
+
+  intervalID = setInterval(function () {
+    i++;
+
+    myCarousel.querySelector(".carousel-indicators .active span").style.width =
+      i + "%";
+
+    if (i >= 100) {
+      // i = 0; -> just in case
+      carousel.next();
+    }
+  }, 50);
+}
